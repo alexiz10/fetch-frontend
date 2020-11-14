e0af65b6e29b5df3ead750e40f210dfe0ac7aa71
Alexiz Hernandez <alexiz.developer@gmail.com>
2020-11-14 15:43:26 -0700

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.(js)$/, use: 'babel-loader' },
+      {
+        test: /\.s(a|c)ss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ]
+  },
+  mode: 'development',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'public/index.html'
+    })
+  ],
+  devServer: { historyApiFallback: true }
+};
